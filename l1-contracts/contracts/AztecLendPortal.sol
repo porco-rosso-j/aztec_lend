c60ebff7cb903eb5635ddb2b3dbedc03c60f1813
Porco <porcorossoj89@gmail.com>
2023-10-20 14:26:43 +0400

    cleanup

--- l1-contracts/contracts/AztecLendPortal.sol
+++ l1-contracts/contracts/AztecLendPortal.sol
@@ -1,26 +1,18 @@
 // SPDX-License-Identifier: Apache-2.0
-pragma solidity >=0.8.18;
+pragma solidity ^0.8.20;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
 import {IRegistry} from "@aztec/l1-contracts/src/core/interfaces/messagebridge/IRegistry.sol";
 import {DataStructures} from "@aztec/l1-contracts/src/core/libraries/DataStructures.sol";
 import {Hash} from "@aztec/l1-contracts/src/core/libraries/Hash.sol";
-
-// docs:start:setup
-import {TokenPortal} from "../TokenPortal.sol";
+import {TokenPortal} from "./TokenPortal.sol";
 import {ISavingsDai} from "./interfaces/ISavingsDai.sol";
 import {IcUsdc} from "./interfaces/IcUsdc.sol";
 
 /**
- * @title UniswapPortal
- * @author Aztec Labs
- * @notice A minimal portal that allow an user inside L2, to withdraw asset A from the Rollup
- * swap asset A to asset B, and deposit asset B into the rollup again.
- * Relies on Uniswap for doing the swap, TokenPortals for A and B to get and send tokens
- * and the message boxes (inbox & outbox).
+ * @notice A portal that allows users to deposit DAI into Spark and USDC into Compound V2 from Aztec.
  */
-contract SavingsDaiPortal {
+contract AztecLendPortal {
     ISavingsDai public constant sDAI =
         ISavingsDai(0x83F20F44975D03b1b09e64809B757c47f942BEeA);
     address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
@@ -28,6 +20,8 @@ contract SavingsDaiPortal {
     IcUsdc public constant cUSDC =
         IcUsdc(0x39AA39c021dfbaE8faC545936693aC917d5E7563);
     address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+
+    error UnsupportedAsset(IERC20);
 
     IRegistry public registry;
     bytes32 public l2StrategyAddress;
@@ -40,25 +34,10 @@ contract SavingsDaiPortal {
         l2StrategyAddress = _l2StrategyAddress;
     }
 
-    // Using a struct here to avoid stack too deep errors
-    struct LocalSwapVars {
-        IERC20 inputAsset;
-        IERC20 outputAsset;
-        bytes32 contentHash;
-    }
-
-    error UnsupportedAsset(IERC20);
-
-    // docs:end:setup
-
-    // docs:start:solidity_uniswap_swap_public
     /**
-     * @notice Exit with funds from L2, perform swap on L1 and deposit output asset to L2 again publicly
-     * @dev `msg.value` indicates fee to submit message to inbox. Currently, anyone can call this method on your behalf.
-     * They could call it with 0 fee causing the sequencer to never include in the rollup.
-     * In this case, you will have to cancel the message and then make the deposit later
+     * @notice Exit with funds from L2, deposit into vault, and send vault token to L2 again publicly
      * @param _inputTokenPortal - The ethereum address of the input token portal
-     * @param _inAmount - The amount of assets to swap (same amount as withdrawn from L2)
+     * @param _inAmount - The amount of assets to deposit (same amount as withdrawn from L2)
      * @param _outputTokenPortal - The ethereum address of the output token portal
      * @param _aztecRecipient - The aztec address to receive the output assets
      * @param _secretHashForL1ToL2Message - The hash of the secret consumable message. The hash should be 254 bits (so it can fit in a Field element)
@@ -77,46 +56,39 @@ contract SavingsDaiPortal {
         address _canceller,
         bool _withCaller
     ) public payable returns (bytes32) {
-        LocalSwapVars memory vars;
-
-        vars.inputAsset = TokenPortal(_inputTokenPortal).underlying();
-        vars.outputAsset = TokenPortal(_outputTokenPortal).underlying();
+        IERC20 inputAsset = TokenPortal(_inputTokenPortal).underlying();
+        IERC20 outputAsset = TokenPortal(_outputTokenPortal).underlying();
 
         // Withdraw the input asset from the portal
         TokenPortal(_inputTokenPortal).withdraw(address(this), _inAmount, true);
-        {
-            // prevent stack too deep errors
-            vars.contentHash = Hash.sha256ToField(
-                abi.encodeWithSignature(
-                    "depositPublic(address,uint256,address,bytes32,bytes32,uint32,address,bool)",
-                    _inputTokenPortal,
-                    _inAmount,
-                    _outputTokenPortal,
-                    _aztecRecipient,
-                    _secretHashForL1ToL2Message,
-                    _deadlineForL1ToL2Message,
-                    _canceller,
-                    _withCaller ? msg.sender : address(0)
-                )
-            );
-        }
-
+        // prevent stack too deep errors
+        bytes32 contentHash = Hash.sha256ToField(
+            abi.encodeWithSignature(
+                "depositPublic(address,uint256,address,bytes32,bytes32,uint32,address,bool)",
+                _inputTokenPortal,
+                _inAmount,
+                _outputTokenPortal,
+                _aztecRecipient,
+                _secretHashForL1ToL2Message,
+                _deadlineForL1ToL2Message,
+                _canceller,
+                _withCaller ? msg.sender : address(0)
+            )
+        );
 
         // Consume the message from the outbox
         registry.getOutbox().consume(
             DataStructures.L2ToL1Msg({
                 sender: DataStructures.L2Actor(l2StrategyAddress, 1),
                 recipient: DataStructures.L1Actor(address(this), block.chainid),
-                content: vars.contentHash
+                content: contentHash
             })
         );
 
-        uint256 amountOut = _depositInternal(vars.inputAsset, _inAmount);
+        uint256 amountOut = _depositInternal(inputAsset, _inAmount);
 
         // approve the output token portal to take funds from this contract
-        // Note, safeApprove was deprecated from Oz
-        vars.outputAsset.approve(address(_outputTokenPortal), amountOut);
-        //sDAI.approve(address(_outputTokenPortal), amountOut);
+        outputAsset.approve(address(_outputTokenPortal), amountOut);
 
         // Deposit the output asset to the L2 via its portal
         return
@@ -131,16 +103,10 @@ contract SavingsDaiPortal {
             );
     }
 
-    // docs:end:solidity_uniswap_swap_public
-
-    // docs:start:solidity_uniswap_swap_private
     /**
-     * @notice Exit with funds from L2, perform swap on L1 and deposit output asset to L2 again privately
-     * @dev `msg.value` indicates fee to submit message to inbox. Currently, anyone can call this method on your behalf.
-     * They could call it with 0 fee causing the sequencer to never include in the rollup.
-     * In this case, you will have to cancel the message and then make the deposit later
+     * @notice Exit with funds from L2, deposit into vault, and send vault token to L2 again privately
      * @param _inputTokenPortal - The ethereum address of the input token portal
-     * @param _inAmount - The amount of assets to swap (same amount as withdrawn from L2)
+     * @param _inAmount - The amount of assets to deposit (same amount as withdrawn from L2)
      * @param _outputTokenPortal - The ethereum address of the output token portal
      * @param _secretHashForRedeemingMintedNotes - The hash of the secret to redeem minted notes privately on Aztec. The hash should be 254 bits (so it can fit in a Field element)
      * @param _secretHashForL1ToL2Message - The hash of the secret consumable message. The hash should be 254 bits (so it can fit in a Field element)
@@ -159,44 +125,39 @@ contract SavingsDaiPortal {
         address _canceller,
         bool _withCaller
     ) public payable returns (bytes32) {
-        LocalSwapVars memory vars;
-
-        vars.inputAsset = TokenPortal(_inputTokenPortal).underlying();
-        vars.outputAsset = TokenPortal(_outputTokenPortal).underlying();
+        IERC20 inputAsset = TokenPortal(_inputTokenPortal).underlying();
+        IERC20 outputAsset = TokenPortal(_outputTokenPortal).underlying();
 
         // Withdraw the input asset from the portal
         TokenPortal(_inputTokenPortal).withdraw(address(this), _inAmount, true);
-        {
-            // prevent stack too deep errors
-            vars.contentHash = Hash.sha256ToField(
-                abi.encodeWithSignature(
-                    "depositPrivate(address,uint256,address,bytes32,bytes32,uint32,address,bool)",
-                    _inputTokenPortal,
-                    _inAmount,
-                    _outputTokenPortal,
-                    _secretHashForRedeemingMintedNotes,
-                    _secretHashForL1ToL2Message,
-                    _deadlineForL1ToL2Message,
-                    _canceller,
-                    _withCaller ? msg.sender : address(0)
-                )
-            );
-        }
+        bytes32 contentHash = Hash.sha256ToField(
+            abi.encodeWithSignature(
+                "depositPrivate(address,uint256,address,bytes32,bytes32,uint32,address,bool)",
+                _inputTokenPortal,
+                _inAmount,
+                _outputTokenPortal,
+                _secretHashForRedeemingMintedNotes,
+                _secretHashForL1ToL2Message,
+                _deadlineForL1ToL2Message,
+                _canceller,
+                _withCaller ? msg.sender : address(0)
+            )
+        );
 
         // Consume the message from the outbox
         registry.getOutbox().consume(
             DataStructures.L2ToL1Msg({
                 sender: DataStructures.L2Actor(l2StrategyAddress, 1),
                 recipient: DataStructures.L1Actor(address(this), block.chainid),
-                content: vars.contentHash
+                content: contentHash
             })
         );
 
-        uint256 amountOut = _depositInternal(vars.inputAsset, _inAmount);
+        uint256 amountOut = _depositInternal(inputAsset, _inAmount);
 
         // approve the output token portal to take funds from this contract
         // Note, safeApprove was deprecated from Oz
-        vars.outputAsset.approve(address(_outputTokenPortal), amountOut);
+        outputAsset.approve(address(_outputTokenPortal), amountOut);
 
         // Deposit the output asset to the L2 via its portal
         return
@@ -211,7 +172,10 @@ contract SavingsDaiPortal {
             );
     }
 
-    function _depositInternal(IERC20 inAsset, uint256 inAmount) private returns (uint256 outAmount) {
+    function _depositInternal(
+        IERC20 inAsset,
+        uint256 inAmount
+    ) private returns (uint256 outAmount) {
         if (address(inAsset) == DAI) {
             inAsset.approve(address(sDAI), inAmount);
             outAmount = sDAI.deposit(inAmount, address(this));
@@ -228,7 +192,10 @@ contract SavingsDaiPortal {
         }
     }
 
-    function _withdrawInternal(IERC20 inAsset, uint256 inAmount) private returns (uint256 outAmount) {
+    function _withdrawInternal(
+        IERC20 inAsset,
+        uint256 inAmount
+    ) private returns (uint256 outAmount) {
         if (address(inAsset) == address(sDAI)) {
             outAmount = sDAI.redeem(inAmount, address(this), address(this));
         } else if (address(inAsset) == address(cUSDC)) {
